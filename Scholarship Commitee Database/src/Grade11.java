bcc52ab0fcaac49797cda6177b9fad53c6e80e99
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2018-01-13 16:18:16 -0500

    Update Grade11.java

--- Scholarship Commitee Database/src/Grade11.java
+++ Scholarship Commitee Database/src/Grade11.java
@@ -1,8 +1,15 @@
-/**
- * 
- * @author sujeethan
- *
- */
-public class Grade11 extends Student{
+
+public class Grade11 extends Student {
+
+	public Grade11(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp) {
+		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
+	}
+	
+	public String toString() {
+		String s = super.toString();
+		s += "Grade 11";
+		return s;
+	}
+	
 
 }
