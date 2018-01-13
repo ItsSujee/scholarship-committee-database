6e86f0790526df6a8b6d7bdd28940ab78e51d01d
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2018-01-13 15:53:33 -0500

    Update Student.java

--- Scholarship Commitee Database/src/Student.java
+++ Scholarship Commitee Database/src/Student.java
@@ -1,8 +1,49 @@
-/**
- * 
- * @author sujeethan
- *
- */
-public abstract class Student {
 
+public abstract class Student {
+	private String fName;
+	private String lName;
+	private int studentNum;
+	private String dateOfBirth;
+	private double avgMarks;
+	private int volHrs;
+	private int numECs;
+	private int hrsEmplyed;
+
+	public Student(int stuNumber, String fNme, String lNme, String DOB,  double avgMrks, int vlHrs, int nmECs, int hrsEmp) {
+		this.fName = fNme;
+		this.lName = lNme;
+		this.studentNum = stuNumber;
+		this.dateOfBirth = DOB;
+		this.avgMarks = avgMrks;
+		this.volHrs = vlHrs;
+		this.numECs = nmECs;
+		this.hrsEmplyed = hrsEmp;
+	}
+	public int getStudentNum() {
+		return this.studentNum;
+	}
+	
+	public void changeAvg(int newAvg){
+		this.avgMarks = newAvg;
+	}
+	public void changeVolHrs(int newHrs){
+		this.volHrs = newHrs;
+	}
+	public void changeECs(int newECs){
+		this.avgMarks = newECs;
+	}
+	public void changeHrsEmplyed(int newHrs){
+		this.hrsEmplyed = newHrs;
+	}
+	public String toString(){
+		String s = "First name: " + fName + "\n";
+		s += "Last name: " + lName + "\n";
+		s += "Student Number: " + studentNum + "\n";
+		s += "Date of birth: " + dateOfBirth + "\n";
+		s += "Average marks: " + avgMarks + "\n";
+		s += "volHrs: " + volHrs + "\n";
+		s += "Number of extracurriculars: " + numECs + "\n";
+		s += "Hours employed: " + hrsEmplyed + "\n";
+		return s;
+	}
 }
