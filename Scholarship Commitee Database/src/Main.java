6172c5c489e948963ba19bd99fd921061bb1c2fb
sujeethan <sujeethan@192.168.2.10>
2018-01-13 02:42:39 -0500

    Added Comment

--- Scholarship Commitee Database/src/Main.java
+++ Scholarship Commitee Database/src/Main.java
@@ -1,4 +1,10 @@
-
+/**
+ * This is the main file that will run the entire program
+ * It will ask the user what command to run, then it will run those commands and return values as necessary
+ * 
+ * @author Sujeethan Vigneswaran
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
