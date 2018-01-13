010caa2b56974e2b5b9c5b15db18b0ca9319e4fd
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2018-01-13 16:17:49 -0500

    Update Grade10.java

--- Scholarship Commitee Database/src/Grade10.java
+++ Scholarship Commitee Database/src/Grade10.java
@@ -1,8 +1,17 @@
-/**
- * 
- * @author sujeethan
- *
- */
-public class Grade10 extends Student{
+
+public class Grade10 extends Student {
+	
+	private int OSSLT;
+	
+	public Grade10(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, int osslt) {
+		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
+		this.OSSLT = osslt; 
+	}
+	public String toString() {
+		String s = super.toString();
+		s += "OSSLT score: " + OSSLT + "\n";
+		s += "Grade 10";
+		return s;
+	}
 
 }
