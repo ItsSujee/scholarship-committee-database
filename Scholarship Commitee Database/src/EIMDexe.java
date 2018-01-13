0a32cd133a5a49a1a17f2affd959403072d47b6d
sujeethan <sujeethan@192.168.2.10>
2018-01-13 18:10:19 -0500

    Structure complete

--- Scholarship Commitee Database/src/EIMDexe.java
+++ Scholarship Commitee Database/src/EIMDexe.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
  * This is the main file that will run the entire program
  * It will ask the user what command to run, then it will run those commands and return values as necessary
  * 
- * 
- * 
  * @author Sujeethan Vigneswaran
  */
 public class EIMDexe {
