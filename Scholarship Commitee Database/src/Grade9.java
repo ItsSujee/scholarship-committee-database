3e0c22e6117e9611c32f7cc20c47ba08630848d1
aarsh2000 <32023476+aarsh2000@users.noreply.github.com>
2018-01-14 02:42:10 -0500

    Update Grade9.java

--- Scholarship Commitee Database/src/Grade9.java
+++ Scholarship Commitee Database/src/Grade9.java
@@ -1,16 +1,32 @@
 public class Grade9 extends Student {
+
 	
-	private int mathEQAO;
+
+	static int mathEQAO;
+
 	
+
 	public Grade9(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, int mEQAO) {
+
 		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
-		this.mathEQAO = mEQAO;
+
+		Grade9.mathEQAO = mEQAO;
+
 	}
+	
+
 	public String toString() {
+
 		String s = super.toString();
+
 		s += "Math EQAO score: " + mathEQAO + "\n";
+
 		s += "Grade 9";
+
 		return s;
+
 	}
+
+
 
 }
