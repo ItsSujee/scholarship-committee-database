7f8360951dc0f157acdb73ec4e4dd8c61c002930
aarsh2000 <32023476+aarsh2000@users.noreply.github.com>
2018-01-14 02:44:39 -0500

    Update Grade12.java

--- Scholarship Commitee Database/src/Grade12.java
+++ Scholarship Commitee Database/src/Grade12.java
@@ -1,21 +1,35 @@
 
 public class Grade12 extends Student {
+
 	
-	private String topPostChoice;
+
+ static String topPostChoice;
+
 	
+
 	public Grade12(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, String tPC) {
+
 		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
-		this.topPostChoice = tPC;
+
+		Grade12.topPostChoice = tPC;
+
 	}
+
 	
-	public void changePostChoice(String newChoice) {
-		this.topPostChoice = newChoice;
-	}
+
+
 	
+
 	public String toString() {
+
 		String s = super.toString();
+
 		s += "Top university choice: " + topPostChoice + "\n";
+
 		s += "Grade 12";
+
 		return s;
+
 	}
+
 }
