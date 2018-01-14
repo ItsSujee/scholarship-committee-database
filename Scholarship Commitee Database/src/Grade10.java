606ebc0c21bf36850f28e063c8899fd98be1601f
aarsh2000 <32023476+aarsh2000@users.noreply.github.com>
2018-01-14 02:42:57 -0500

    Update Grade10.java

--- Scholarship Commitee Database/src/Grade10.java
+++ Scholarship Commitee Database/src/Grade10.java
@@ -1,17 +1,33 @@
-
 public class Grade10 extends Student {
+
 	
-	private int OSSLT;
+
 	
+	static int OSSLT;
+
+	
+
 	public Grade10(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, int osslt) {
+
 		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
-		this.OSSLT = osslt; 
+
+		Grade10.OSSLT = osslt; 
+
 	}
+
+
 	public String toString() {
+
 		String s = super.toString();
+
 		s += "OSSLT score: " + OSSLT + "\n";
+
 		s += "Grade 10";
+
 		return s;
+
 	}
+
+
 
 }
