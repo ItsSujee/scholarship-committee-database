42a965880eda9fe538bdccddb5a0811e80fb65fb
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2018-01-15 09:57:04 -0500

    Update Grade11.java

--- Scholarship Commitee Database/src/Grade11.java
+++ Scholarship Commitee Database/src/Grade11.java
@@ -3,22 +3,37 @@
  * @author 
  *
  */
+
 public class Grade11 extends Student {
 
-	public Grade11(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp) {
-
+	int mathContestScore;	
+	
+	public Grade11(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, int mathScore) {
 		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
-
+		this.mathContestScore = mathScore;
 	}
 
+	/**
+	 * Returns -1 if parameter has greater mathContest score
+	 * Returns +1 if parameter has less mathContest score
+	 * Returns 0 if parameter has equal mathContest score
+	 * 
+	 * @param s1
+	 */
+	public int compareTo(Student s1) {
+		if (this.mathContestScore > ((Grade11)s1).mathContestScore) {
+			return +1;
+		} else if (this.mathContestScore < ((Grade11)s1).mathContestScore) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	
 	public String toString() {
-
 		String s = super.toString();
-
 		s += "Grade 11";
-
 		return s;
-
 	}
-
+	
 }
