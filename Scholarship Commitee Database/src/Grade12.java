043b56e6a1d0c1bfa12edcb16bcea8c21408b91e
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2018-01-15 09:57:23 -0500

    Update Grade12.java

--- Scholarship Commitee Database/src/Grade12.java
+++ Scholarship Commitee Database/src/Grade12.java
@@ -5,26 +5,36 @@
  */
 public class Grade12 extends Student {
 
- static String topPostChoice;
+	int topPostChoice;
 
-	public Grade12(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, String tPC) {
-
+	public Grade12(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, int topPost) {
 		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
-
-		Grade12.topPostChoice = tPC;
-
+		this.topPostChoice = topPost;
 	}
 
+
+	/**
+	 * Returns -1 if parameter has greater topPostChoice score
+	 * Returns +1 if parameter has less topPostChoice score
+	 * Returns 0 if parameter has equal topPostChoice score
+	 * 
+	 * @param s1
+	 */
+	public int compareTo(Student s1) {
+		if (this.topPostChoice > ((Grade12)s1).topPostChoice) {
+			return +1;
+		} else if (this.topPostChoice < ((Grade12)s1).topPostChoice) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	
 	public String toString() {
-
 		String s = super.toString();
-
-		s += "Top university choice: " + topPostChoice + "\n";
-
+		s += "Top university choice enterence average: " + topPostChoice + "\n";
 		s += "Grade 12";
-
 		return s;
-
 	}
 
 }
