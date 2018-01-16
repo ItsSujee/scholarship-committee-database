a8acf2c6fa623c2029ba278b2d43bdcf3e6d9a38
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2018-01-16 10:15:55 -0500

    MAJOR UPDATE

--- Scholarship Commitee Database/src/EIMDexe.java
+++ Scholarship Commitee Database/src/EIMDexe.java
@@ -1,55 +1,31 @@
 import java.util.Scanner;
 
 /**
-
- * This is the main file that will run the entire program
-
- * It will ask the user what command to run, then it will run those commands and return values as necessary
-
+ * Class Name: EIMDexe
+ * @Author: Sujeethan, Harry, Dhruv, Aarsh
+ * Date: January 16, 2018
+ * School: Lester B. Pearson C.I.
+ * Purpose:
  * 
-
- * @author Sujeethan Vigneswaran
-
+ *
  */
-
 public class EIMDexe {
 
-
-
 	public static void main(String[] args) {
-
-		// TODO Auto-generated method stub
-
-
-
 		//Ask for input
-
 		System.out.println("Welcome to the Educational Institution Management Database; Scholarship Selection Made Easy" + "\n");
 
 		askForInput();
-  
 
 		Scanner sc = new Scanner(System.in);
-
 		int userInput = Integer.parseInt(sc.nextLine());
-
-
 
 		boolean toClose = false;
 
-
-
 		while (toClose != true) {
 
-
-
-			//case statement
-
-			
-
+			//case statement			
 			switch (userInput) {
-
-			
 
 			case 0:
 
