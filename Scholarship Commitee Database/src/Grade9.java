db74e540e720240ae2c3bba33fab42749c6ec3a0
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2018-01-16 10:14:04 -0500

    MAJOR UPDATE

--- Scholarship Commitee Database/src/Grade9.java
+++ Scholarship Commitee Database/src/Grade9.java
@@ -1,16 +1,35 @@
 /**
+ * Class Name: Grade9
+ * @Author: Sujeethan, Harry, Dhruv, Aarsh
+ * Date: January 16, 2018
+ * School: Lester B. Pearson C.I.
+ * Purpose:
  * 
- * @author
  *
  */
 public class Grade9 extends Student {
 
+	/**
+	 * new field that is only for Grade9 Students, EQAO score
+	 */
 	int mathEQAO;
 
+	/**
+	 * Constructor for Grade9 class
+	 * 
+	 * @param stuNumber //integer that is the student number
+	 * @param fNme //String that is the first name
+	 * @param lNme //String that is the last name
+	 * @param DOB //String that is the date of birth format (DD/MM/YY)
+	 * @param avgMrks //double that is the average of the marks
+	 * @param vlHrs //integer that is the number of volunteer hours completed
+	 * @param nmECs //integer that is number of extra curricular activities
+	 * @param hrsEmp //integer that is number of hours employed
+	 * @param mEQAO //integer that is the result of the math EQAO
+	 */
 	public Grade9(int stuNumber, String fNme, String lNme, String DOB, double avgMrks, int vlHrs, int nmECs, int hrsEmp, int mEQAO) {
 		super(stuNumber, fNme, lNme, DOB, avgMrks, vlHrs, nmECs, hrsEmp);
 		this.mathEQAO = mEQAO;
-
 	}
 
 	/**
@@ -18,7 +37,7 @@ public class Grade9 extends Student {
 	 * Returns +1 if parameter has less EQAO score
 	 * Returns 0 if parameter has equal EQAO score
 	 * 
-	 * @param s1
+	 * @param s1 //parameter is a student, must be an instanceOf Grade9
 	 */
 	public int compareTo(Student s1) {
 		if (this.mathEQAO > ((Grade9)s1).mathEQAO) {
@@ -30,6 +49,9 @@ public class Grade9 extends Student {
 		}
 	}
 
+	/**
+	 * toString method, prints out the super from Student, adds the EQAO and the Grade
+	 */
 	public String toString() {
 		String s = super.toString();
 		s += "Math EQAO score: " + mathEQAO + "\n";
